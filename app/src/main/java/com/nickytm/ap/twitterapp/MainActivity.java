b2b58aaf6e4148936f931738c741be99b59b40b3
Remy <Remye98@gmail.com>
2016-05-12 19:59:27 +0200

    test

--- app/src/main/java/com/nickytm/ap/twitterapp/MainActivity.java
+++ app/src/main/java/com/nickytm/ap/twitterapp/MainActivity.java
@@ -4,8 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
-    @Override
+//R
+   @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
